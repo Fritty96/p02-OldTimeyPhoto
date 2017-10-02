e0d91f9a2d4815c2bb1fd8f19814bea114d66d8c
Fritty96 <mphillips39@mail.csuchico.edu>
2017-10-02 16:39:06 -0700

    Project Decomp

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include<string>
+#include<vector>
+#include "bitmap.h"
+
+using namespace std
+
+int main (){
+
+
+/*
+Program Decompisition
+
+    Output Ask User for BMP Picture File
+        Store File if valid 
+        If not valid, tell user and reprompt
+
+
+    Convert User File into Matrix
+    
+    Create Loop
+        Checks RGB of each Pixel And finds average
+        Change Value of RGB so each equals   
+        Store New Value and move onto next pixel
+
+    
+    Save new updated bitmap
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+return 0;
+}
